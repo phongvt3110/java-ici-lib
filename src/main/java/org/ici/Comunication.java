8eef031d6d3e5b4286533969982c5cbeaec74d08
phongvt3110 <vtphong651043@gmail.com>
2019-05-11 09:01:45 +0700

    first commit

---
+++ src/main/java/org/ici/Comunication.java
@@ -0,0 +1,16 @@
+package org.ici;
+
+/**
+ * Created by phongvt on 06/05/2019.
+ */
+public class Comunication {
+    public static void SayHello() {
+        System.out.println("Hello world");
+    }
+    public void SayHello(String str) {
+        System.out.println("Hello " + str);
+    }
+    public void SayGoodbye(String str) {
+        System.out.println("Goodbye " + str);
+    }
+}
---
+++ src/main/java/org/ici/Logger.java
@@ -0,0 +1,10 @@
+package org.ici;
+
+/**
+ * Created by phongvt on 11/05/2019.
+ */
+public class Logger {
+    public static void info(String message) {
+        System.out.println("INFO:" + message);
+    }
+}
