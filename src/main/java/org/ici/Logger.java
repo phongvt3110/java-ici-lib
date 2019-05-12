ab3be7c03ad1c0efd354c11475959eb494165da2
phongvt3110 <vtphong651043@gmail.com>
2019-05-12 09:22:48 +0700

    update code

--- src/main/java/org/ici/Logger.java
+++ src/main/java/org/ici/Logger.java
@@ -7,4 +7,7 @@ public class Logger {
     public static void info(String message) {
         System.out.println("INFO:" + message);
     }
+    public static void warning(String message) {
+        System.out.println("WARNING:" + message);
+    }
 }
